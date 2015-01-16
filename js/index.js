13b04a729218b36cad2cc4b913e320eaae76d12d
Verochka <veronika8@bk.ru>
2015-01-16 21:04:25 +0200

    added Function board

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ var togglers = 0;
 var compfishka = 0;
 var myfishka = 0;
 
-function Board() {
+function Board() {(!)
 	board = new Array();
 	for (var i = 0; i < 8; i++) {
 		board[i] = new Array();
